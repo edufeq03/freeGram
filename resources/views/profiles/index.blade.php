a900b07f4e93cbbb6dfa905aac79e18556c61d8b
Eduardo Capela <edufeq03@gmail.com>
2019-12-03 00:24:57 -0300

    Create form for Edit Profile

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,7 +11,9 @@
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user->username }}</h1>
                 <a href="/p/create">Add new Post</a>
+                
             </div>
+            <a href="/profile/{{$user->id}}/edit">Edit Profile</a>
             <div class="d-flex">
                 <div class="pr-3"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-3"><strong>55</strong> followers</div>
@@ -26,7 +28,9 @@
     <div class="row pt-5">
         @foreach($user->posts as $post)
             <div class="col-4 pb-4">
-                <img src="/storage/{{$post->image}}" class="w-100">
+                <a href="/p/{{ $post->id }}">
+                    <img src="/storage/{{$post->image}}" class="w-100">
+                </a>
             </div>
         @endforeach
     </div>
