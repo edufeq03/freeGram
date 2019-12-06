4d417212f79d32b993369e5625d998b3e5be8b19
Eduardo Capela <edufeq03@gmail.com>
2019-12-06 01:42:15 -0300

    adding cache

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 
@@ -14,7 +15,11 @@ class PostsController extends Controller
 
     public function index()
     {
-        return view('welcome');
+        $users = auth()->user()->following()->pluck('profiles.user_id');
+
+        $posts = Post::whereIn('user_id', $users)->with('user')->latest()->paginate(5);
+
+        return view('posts.index', compact('posts'));
     }
 
     public function create()
